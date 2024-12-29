43ab3e32e6512c8811d062c6baa439f3ae0634a3
Peter Kokot <peterkokot@gmail.com>
2024-12-29 02:08:33 +0100

    Update PHP to 8.4 and update tools
    - PHP 8.4
    - Added petk/normalizator to check for common CS issues in the
      Git repository
    - Updated .github/sort.php script
    - CS synced in README.md

--- .github/sort.php
+++ .github/sort.php
@@ -1,6 +1,42 @@
 #!/usr/bin/env php
 <?php
 
+declare(strict_types=1);
+
+// Check requirements.
+(static function () {
+    if (PHP_VERSION_ID < 80300) {
+        fwrite(
+            STDERR,
+            'Error: This script requires PHP 8.3 or greater. Please update '
+            . "your PHP version.\n",
+        );
+
+        exit(1);
+    }
+
+    // Check required PHP extensions.
+    $missingExtensions = [];
+
+    foreach (['intl'] as $extension) {
+        if (!extension_loaded($extension)) {
+            $missingExtensions[] = $extension;
+        }
+    }
+
+    if ([] !== $missingExtensions) {
+        fwrite(
+            STDERR,
+            sprintf(
+                "Error: Missing PHP extensions: %s.\n",
+                implode(', ', $missingExtensions),
+            ),
+        );
+
+        exit(1);
+    }
+})();
+
 function init(array $argv): int
 {
     $options = [
@@ -14,15 +50,16 @@ function init(array $argv): int
     $footer = array_slice($contents, -5);
     $lines = array_slice($contents, 15, -5);
 
-    if(end($header) !== "\n") {
+    if ("\n" !== end($header)) {
         echo $options['red'] . "The README.md file needs to be corrected.\n" . $options['default'];
+
         return 1;
     }
 
-    $collator = new \Collator('en_US');
+    $collator = new Collator('en_US');
 
     // Sort the array using PHP Intl Collator.
-    $collator->asort($lines, \Collator::SORT_STRING);
+    $collator->asort($lines, Collator::SORT_STRING);
 
     $result = array_merge($header, $lines, $footer);
 
@@ -35,6 +72,7 @@ function init(array $argv): int
         return 0;
     } else {
         echo $options['red'] . "The README.md file needs to be sorted alphabetically.\n" . $options['default'];
+
         return 1;
     }
 }
